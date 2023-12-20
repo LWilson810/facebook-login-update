2313c864c34f6c7b4802d98ca100221e5d8b989b
LWilson810 <L.Wilson10@outlook.com>
2023-12-20 17:48:11 +0900

    update [...nextauth]

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -12,12 +12,18 @@ const options = {
     callbacks: {
       async signIn(user, account, profile) {
         // Customize the signIn callback as needed
+        console.log("------------------ Signin -----------------")
+        console.log(user, account, profile)
         return true;
       },
   
       async redirect(url, baseUrl) {
         // Customize the redirect callback to specify the desired URL
         return '/dashboard';
+      },
+
+      async session(session, user) {
+        return session
       },
     },
       // Set the callback URL to match the redirect URI in the Facebook Developer portal
