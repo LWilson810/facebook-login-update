f7b8317b240ef6bb21dc1bdb0289d53fd9791268
LWilson810 <L.Wilson10@outlook.com>
2023-12-21 13:51:02 +0900

    1231231231

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,13 +8,13 @@ export const options = {
           clientId: "777565144211400",
           clientSecret: "b25b284325a674258641e6678a0e68e6"
         }),
-        TwitterProvider({
-          clientId: "R0ZDWWFMd050Y1R5blFuRkVDTnQ6MTpjaQ",
-          clientSecret: "piLD3tJmJyXX6KW9eYMeOSm8Ry7AVAzHstUpXGJCTRyv9kvkGT",
-          version: '2.0',
-          accessTokenUrl: 'https://api.twitter.com/oauth2/token',
+      //   TwitterProvider({
+      //     clientId: "R0ZDWWFMd050Y1R5blFuRkVDTnQ6MTpjaQ",
+      //     clientSecret: "piLD3tJmJyXX6KW9eYMeOSm8Ry7AVAzHstUpXGJCTRyv9kvkGT",
+      //     version: '2.0',
+      //     accessTokenUrl: 'https://api.twitter.com/oauth2/token',
           
-      })
+      // })
     ],
     secret: "v7COYqKpEdnCbd5aISAw9BxjupOLKYCgBVZ2kwusMNs=", 
     callbacks: {
